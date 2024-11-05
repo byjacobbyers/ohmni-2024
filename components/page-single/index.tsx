8aaf2ae2cd6d08ad932b8587fef00b19d0acf3aa
byebyers <byers.jacob@gmail.com>
2024-11-05 14:37:11 -0700

    mobile padding

--- components/page-single/index.tsx
+++ components/page-single/index.tsx
@@ -18,7 +18,7 @@ export default function Page({ page }: { page: SanityDocument }) {
   return (
     <>
       <Header items={pageNav?.header} />
-      <main className="flex min-h-screen flex-col items-center gap-y-24 py-24 2xl:pt-48">
+      <main className="flex min-h-screen flex-col items-center gap-y-24 py-12 lg:py-24 2xl:pt-48">
         <Sections body={sections} />
       </main>
       <Footer items={pageNav?.footer} />
