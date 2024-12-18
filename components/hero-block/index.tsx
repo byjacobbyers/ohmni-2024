848b071433fa59b01b8fd6abbe9564ef8a591636
bybyers <byers.jacob@gmail.com>
2024-12-17 20:03:37 -0700

    hero cta

--- components/hero-block/index.tsx
+++ components/hero-block/index.tsx
@@ -48,6 +48,48 @@ const HeroBlock: React.FC<HeroBlockType> = ({
             }}
           >
             <SimpleText content={content} />
+            {cta && cta.active && (
+              <motion.div 
+                className='flex justify-center md:justify-start'
+                initial={{ 
+                  opacity: 0,
+                  scale: 0.95
+                }}
+                whileInView={{ 
+                  opacity: 1,
+                  scale: 1
+                }}
+                viewport={{ once: true }} 
+                transition={{ 
+                  delay: componentIndex !== 0 ? 0.5 : 0,
+                  type: 'spring',
+                  duration: 1.5
+                }}
+              >
+                <Route data={cta.route} className='flex'>
+                  <motion.div
+                    initial={{ 
+                      scale: 1
+                    }}
+                    whileHover={{ 
+                      scale: 1.05
+                    }}
+                    whileTap={{ 
+                      scale: 0.95
+                    }}
+                    transition={{ 
+                      type: 'spring',
+                      duration: 0.5
+                    }}
+                    className='flex w-full'
+                  >
+                    <Button className={`${cta.plausibleEvent ? `plausible-event-name=${cta.plausibleEvent}` : ''}`}>
+                      {cta?.route?.title ? cta?.route?.title : 'Learn More'}
+                    </Button>
+                  </motion.div>
+                </Route>
+              </motion.div>
+            )}
           </motion.div>
           <motion.div 
             className="w-full aspect-lottie md:w-1/3  mt-0 md:mt-24"
